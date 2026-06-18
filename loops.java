cdda23341810d141804926eebf0e6ad1c31a0fe3
Samreen Bano <sameeen.2005@gmail.com>
2026-06-19 00:47:03 +0530

    started lopps

--- loops.java
+++ loops.java
@@ -1,3 +1,30 @@
-public class loops {
-    
-}
+// public class loops {
+//     public static void main(String[] args) {
+        
+//         for (int i=1; i<10;i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+
+// public class loops{
+//     public static void main(String [] args){
+//         for (int i=1; i<6; i++){
+//             System.out.println("hello world");
+//         }
+//     }
+// }
+
+
+
+// print number from 0 to 10
+
+// public class loops{
+//     public static void main(String []args){
+//         for (int i =0; i <11; i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
