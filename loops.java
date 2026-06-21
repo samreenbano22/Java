6f1116354c48589371ec96340387393420b52da0
Samreen Bano <sameeen.2005@gmail.com>
2026-06-22 01:24:24 +0530

    learnt some exp on loops

--- loops.java
+++ loops.java
@@ -96,3 +96,35 @@
 
 
 
+//print sum of first n natural numbers    
+
+// import java.util.*;
+// public class loops{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         int sum = 0;
+//         for(int i= 0; i<=n; i++){
+//         sum = sum + i;
+// }
+//         System.out.println(sum);
+// }
+// }
+
+
+
+
+
+//print the table of a number input by the user
+// import java.util.*;
+// public class loops{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         for (int i=1; i<=10; i++){
+//             System.out.println(n + " * " + i + " = " + n*i);
+//         }
+//     }
+// }
