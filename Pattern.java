e88993cc5bd5c91cf1a0961758363af0f06703e0
Samreen Bano <sameeen.2005@gmail.com>
2026-06-26 14:28:03 +0530

    practice basic questions on pattern using loops

--- Pattern.java
+++ Pattern.java
@@ -9,6 +9,7 @@
 // }
 
 
+
 //OR IT CAN ALSO BE DONE USING LOOPS CONCEPTS
 // public class Pattern {
 //     public static void main(String[] args) {
@@ -20,6 +21,8 @@
 //         }
 //     }
 // }
+
+
 
 
 //QUESTIONS ON PATTTERNS
@@ -37,6 +40,7 @@
 //             }
 //             System.out.println("*");    //will print the stars in the next line 
 //         } 
+
 //     }
 // }
 
@@ -61,6 +65,7 @@
 //             }
 //             System.out.println();
 //         }
+
 //     }
 // }
     
@@ -68,7 +73,7 @@
 
 // or
 
-//half pyramid pattern
+//half upward pyramid pattern
 
 
 // public class Pattern{
@@ -81,6 +86,7 @@
 //             System.out.println();
 //         }
 //     }
+
 //     }
 
 
@@ -90,14 +96,113 @@
 // **
 // *
 
+// public class Pattern{
+//     public static void main(String [] args){
+//         for(int i=4; i>=1; i--){
+//             for (int j=1; j <=i ; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println("");
+//         }
+//     }
+
+// }
+//or
+
+// public class Pattern{
+//     public static void main(String [] args){
+//         int n =20;
+//         for(int i=n; i>=1; i--){
+//             for (int j=1; j <=i ; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println("");
+//         }
+//     }
+
+// }
+
+
+
+
+//pattern 
+//          *           3 spaces and 1 star
+//         **           2 spaces and 2 stars
+//        ***           1 space and 3 stars
+//       ****           0 spaces and 4 stars
+
+
+// public class Pattern{
+//     public static void main(String [] args){
+//             int n = 4;
+//         for (int i=1; i<=n; i++){   //for rows
+//             for (int j=1; j<=n-i; j++){  //for spaces
+//                 System.out.print(" ");
+//             }
+//             for (int j=1; j<=i; j++){    //for stars
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+// public class Pattern{
+//     public static void main(String [] args){
+//         int n=30;
+//         for (int i=1; i<=n; i++){
+//             for (int j =1 ; j<=i; j++){
+//                 System.out.print(j+ " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class Pattern {
+//     public static void main(String args[]){
+//         int n =40;
+//         for (int i =1; i<=n; i++){
+//             for(int j=1; j<=n-i+1; j++){
+//             System.out.print(j);
+//         }
+//         System.out.println();
+//     }
+// }
+
+// }
+
+// public class Pattern{
+//     public static void main(String [] args){
+//         int n=10;
+//         int number = 1;
+
+//         //outer loop for printing numbers
+//         for (int i =1; i<=n; i++){
+//             for (int j=1; j<=i; j++){
+//                 System.out.print(number+ " ");
+//                 number++;  
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
 public class Pattern{
     public static void main(String [] args){
-        for(int i=4; i>=1; i--){
-            for (int j=1; j <=i ; j++){
-                System.out.print("*");
+        int n=5;
+        for (int i =1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                if((i+j) %2 == 0){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+            }      
+                System.out.println("");
             }
-            System.out.println("");
         }
-
     }
-}
